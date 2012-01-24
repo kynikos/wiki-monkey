d99869316937e0a7f40e5c37615151ffe2280ff4
Dario Giovannetti <dev@dariogiovannetti.com>
2012-01-25 00:00:53 +0100

    Issue #13: working default configuration Add release checklist

--- src/wikiMonkey.user.js
+++ src/wikiMonkey.user.js
@@ -2,11 +2,14 @@
 // @name Wiki Monkey
 // @namespace https://github.com/kynikos/wiki-monkey
 // @description Perform automatic actions when editing wiki pages
-// @version 1.4.1
+// @version development
 // @icon http://github.com/downloads/kynikos/wiki-monkey/wiki-monkey.png
 // @match http://*.wikipedia.org/*
 // @match https://wiki.archlinux.org/*
-// @require https://raw.github.com/kynikos/wiki-monkey/master/src/wikiMonkeyCore.js
+// @require https://raw.github.com/kynikos/wiki-monkey/development/src/wikiMonkeyCore.js
+// @require https://raw.github.com/kynikos/wiki-monkey/development/src/user-functions/archWikiAppendDiffToRCPatrol.js
+// @require https://raw.github.com/kynikos/wiki-monkey/development/src/user-functions/archWikiNewTemplates.js
+// @require https://raw.github.com/kynikos/wiki-monkey/development/src/user-functions/multipleLineBreaks.js
 // ==/UserScript==
 
 /*
@@ -32,7 +35,7 @@
  *             [
  *                 Level 4: this level defines the button for a function: its
  *                 first element is the function name, while the second element
- *                 is the text dislpayed on the button; a third optional
+ *                 is the text displayed on the button; a third optional
  *                 boolean value can be used to create a text input field next
  *                 to the button, which should be accessed from the user
  *                 function with "this.nextSibling.value".
@@ -41,42 +44,20 @@
  *     ]
  * ]
  * 
- * The functions in each row
- * will be executed in the order they are added.
- * 
- * For example:
- *
- * main([
- *     [
- *         [
- *             [UF_myFunction1, "myFunction1"],
- *             [UF_myFunction2, "myFunction2", true]
- *         ],
- *         [
- *             [UF_myFunction3, "myFunction3", true],
- *             [UF_myFunction4, "myFunction4"]
- *         ]
- *     ],
- *     [
- *         [
- *             [UF_myFunction5, "myFunction5"],
- *             [UF_myFunction6, "myFunction6"]
- *         ],
- *         [
- *             [UF_myFunction7, "myFunction7"],
- *             [UF_myFunction8, "myFunction8", true]
- *         ]
- *     ],
- * 
- *     ...
- * 
- * ]);
+ * The functions in each row are executed in the order they are added.
  *
  */
 
 main([
     [
-        [],
-        []
+        [
+            [UF_archWikiNewTemplates, "Update templates"],
+            [UF_multipleLineBreaks, "Multiple line breaks"]
+        ]
+    ],
+    [
+        [
+            [UF_archWikiAppendDiffToRCPatrol, "Append to patrol list", true]
+        ]
     ]
 ]);
