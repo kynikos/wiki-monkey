23580a4cb423fcf54afec1ff8440d5f18586b775
Dario Giovannetti <dev@dariogiovannetti.com>
2012-01-26 23:53:17 +0100

    switch to master branch

--- src/WikiMonkey.user.js
+++ src/WikiMonkey.user.js
@@ -6,11 +6,11 @@
 // @icon http://github.com/downloads/kynikos/wiki-monkey/wiki-monkey.png
 // @match http://*.wikipedia.org/*
 // @match https://wiki.archlinux.org/*
-// @require https://raw.github.com/kynikos/wiki-monkey/1.5.0/src/WikiMonkeyCore.js
-// @require https://raw.github.com/kynikos/wiki-monkey/1.5.0/src/modules/ArchWikiAppendDiffToRCPatrol.js
-// @require https://raw.github.com/kynikos/wiki-monkey/1.5.0/src/modules/ArchWikiNewTemplates.js
-// @require https://raw.github.com/kynikos/wiki-monkey/1.5.0/src/modules/ExpandContractions.js
-// @require https://raw.github.com/kynikos/wiki-monkey/1.5.0/src/modules/MultipleLineBreaks.js
+// @require https://raw.github.com/kynikos/wiki-monkey/master/src/WikiMonkeyCore.js
+// @require https://raw.github.com/kynikos/wiki-monkey/master/src/modules/ArchWikiAppendDiffToRCPatrol.js
+// @require https://raw.github.com/kynikos/wiki-monkey/master/src/modules/ArchWikiNewTemplates.js
+// @require https://raw.github.com/kynikos/wiki-monkey/master/src/modules/ExpandContractions.js
+// @require https://raw.github.com/kynikos/wiki-monkey/master/src/modules/MultipleLineBreaks.js
 // ==/UserScript==
 
 WM.main(
