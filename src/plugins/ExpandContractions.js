4020d655539dbb4192950207c8eba0d3658dfd08
Dario Giovannetti <dev@dariogiovannetti.com>
2012-02-08 14:30:35 +0100

    #25: ExpandContractions doesn't preserve capitalization for "can't"

--- src/plugins/ExpandContractions.js
+++ src/plugins/ExpandContractions.js
@@ -5,10 +5,11 @@ WM.Plugins.ExpandContractions = new function () {
         
         // Note that 's and 'd are ambiguous
         // Note that JavaScript doesn't support look behind :(
+        // Pay attention to preserve the original capitalization
         s = s.replace(/(you|we|they)'re/ig, '$1 are');
         s = s.replace(/(you|we|they)'ve/ig, '$1 have');
         s = s.replace(/(is|are|have|has|do|does)n't/ig, '$1 not');
-        s = s.replace(/can't/ig, 'cannot');
+        s = s.replace(/(c)an't/ig, '$1annot');
         
         WM.Editor.writeSource(s);
         
