abe28fc8b98414f72232a63b3e4cedfe864a562c
Dario Giovannetti <dev@dariogiovannetti.com>
2012-02-13 23:38:23 +0100

    Templates Package Official/AUR are extinct

--- src/plugins/ArchWikiNewTemplates.js
+++ src/plugins/ArchWikiNewTemplates.js
@@ -15,9 +15,6 @@ WM.Plugins.ArchWikiNewTemplates = new function () {
         var re15 = /&lt;tt&gt;(((?!&lt;(tt|nowiki)&gt;)[^\|\n])*?((?!&lt;(tt|nowiki)&gt;)[^\|\}\n]))&lt;\/tt&gt;/ig;
         var re16 = /&lt;tt&gt;(((?!&lt;(tt|nowiki)&gt;)[^\n])+?)&lt;\/tt&gt;/ig;
         
-        var re17 = /\{\{ *[Pp]ackage Official *\|/g;
-        var re18 = /\{\{ *[Pp]ackage AUR *\|/g;
-        
         s = s.replace(re8, '{{bc|$1}}');
         s = s.replace(re9, '{{bc|1=$1}}'); // Must come after re8
         s = s.replace(re10, '{{bc|<nowiki>$1</nowiki>}}'); // Must come after re9
@@ -30,21 +27,16 @@ WM.Plugins.ArchWikiNewTemplates = new function () {
         s = s.replace(re15, '{{ic|1=$1}}'); // Must come after re14
         s = s.replace(re16, '{{ic|<nowiki>$1</nowiki>}}'); // Must come after re15
         
-        s = s.replace(re17, '{{Pkg|');
-        s = s.replace(re18, '{{AUR|');
-        
         WM.Editor.writeSource(s);
         
         if (s != original) {
-            WM.Log.logInfo("Updated deprecated templates and HTML tags");
+            WM.Log.logInfo("Turned HTML tags into proper templates");
         }
         
         var tests = [
             ['&lt;pre>', s.match(/&lt;pre/ig)],
             ['&lt;code>', s.match(/&lt;code/ig)],
-            ['&lt;tt>', s.match(/&lt;tt/ig)],
-            ['Package Official', s.match(/\{\{ *[Pp]ackage Official/g)],
-            ['Package AUR', s.match(/\{\{ *[Pp]ackage AUR/g)]
+            ['&lt;tt>', s.match(/&lt;tt/ig)]
         ]
         
         for each (var test in tests) { 
