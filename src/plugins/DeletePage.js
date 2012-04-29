b7590d563c2f130450123f99b399bc7312d284a1
Dario Giovannetti <dev@dariogiovannetti.net>
2012-04-29 18:24:47 +0200

    summary as an argument

--- src/plugins/DeletePage.js
+++ src/plugins/DeletePage.js
@@ -1,12 +1,12 @@
 WM.Plugins.DeletePage = new function () {
     this.mainAuto = function (args, title) {
+        var summary = args[0];
+        
         var page = WM.MW.callQuery({prop: 'info',
                                     intoken: 'delete',
                                     titles: encodeURIComponent(title)});
         
         var deletetoken = page.deletetoken;
-        
-        var summary = "Turkish articles moved to http://archtr.org/wiki/";
         
         var res = WM.MW.callAPIPost({action: 'delete',
                                      bot: '1',
@@ -20,6 +20,31 @@ WM.Plugins.DeletePage = new function () {
         }
         else {
             return true;
+        }
+    };
+    
+    this.main = function (args) {
+        var [title, summary] = args;
+        
+        WM.Log.logInfo("Deleting " + title + "...");
+        
+        var page = WM.MW.callQuery({prop: 'info',
+                                    intoken: 'delete',
+                                    titles: encodeURIComponent(title)});
+        
+        var deletetoken = page.deletetoken;
+        
+        var res = WM.MW.callAPIPost({action: 'delete',
+                                     bot: '1',
+                                     title: encodeURIComponent(title),
+                                     token: encodeURIComponent(deletetoken),
+                                     reason: encodeURIComponent(summary)});
+        
+        if (!res['delete']) {
+            WM.Log.logError(cat + " has not been deleted!\n" + res['error']['info'] + " (" + res['error']['code'] + ")");
+        }
+        else {
+            WM.Log.logInfo(title + " deleted");
         }
     };
 };
