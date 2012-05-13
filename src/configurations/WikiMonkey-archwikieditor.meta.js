89fac70e4e737d53bff575ecc9c6ea84faacb87d
Dario Giovannetti <dev@dariogiovannetti.net>
2012-05-13 22:47:11 +0200

    prepare 1.10.3

--- src/configurations/WikiMonkey-archwikieditor.meta.js
+++ src/configurations/WikiMonkey-archwikieditor.meta.js
@@ -1,3 +1,3 @@
 // ==UserScript==
-// @version 16dev-archwikieditor
+// @version 1.10.3-archwikieditor
 // ==/UserScript==
--- src/configurations/WikiMonkey-archwikipatrol.meta.js
+++ src/configurations/WikiMonkey-archwikipatrol.meta.js
@@ -1,3 +1,3 @@
 // ==UserScript==
-// @version 16dev-archwikipatrol
+// @version 1.10.3-archwikipatrol
 // ==/UserScript==
