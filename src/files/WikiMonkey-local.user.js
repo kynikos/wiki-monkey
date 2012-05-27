b5e0bb4c43c3e97b87c5f31d574b415b2ba7daf9
Dario Giovannetti <dev@dariogiovannetti.net>
2012-05-27 02:58:12 +0200

    fix email

--- src/files/WikiMonkey-local.user.js
+++ src/files/WikiMonkey-local.user.js
@@ -2,7 +2,7 @@
 // @id wiki-monkey-local
 // @name Wiki Monkey
 // @namespace https://github.com/kynikos/wiki-monkey
-// @author Dario Giovannetti <dev@dariogiovannetti.com>
+// @author Dario Giovannetti <dev@dariogiovannetti.net>
 // @version local
 // @description MediaWiki-compatible bot and editor assistant that runs in the browser
 // @website https://github.com/kynikos/wiki-monkey
