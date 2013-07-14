30f041b9e281eade847ad6e120c66e66bcee7122
Dario Giovannetti <dev@dariogiovannetti.net>
2013-07-14 16:01:55 +0800

    #99: Log: no horizontal scrolling

--- src/modules/Log.js
+++ src/modules/Log.js
@@ -1,19 +1,19 @@
 /*
  *  Wiki Monkey - MediaWiki bot and editor assistant that runs in the browser
  *  Copyright (C) 2011-2013 Dario Giovannetti <dev@dariogiovannetti.net>
- * 
+ *
  *  This file is part of Wiki Monkey.
- * 
+ *
  *  Wiki Monkey is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
- * 
+ *
  *  Wiki Monkey is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
- * 
+ *
  *  You should have received a copy of the GNU General Public License
  *  along with Wiki Monkey.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -22,59 +22,59 @@ WM.Log = new function () {
     this._makeLogArea = function () {
         var log = document.createElement('div');
         log.id = 'WikiMonkeyLog';
-        
+
         GM_addStyle("#WikiMonkeyLog {height:10em; border:2px solid #07b; padding:0.5em; overflow:auto; resize:vertical; background-color:#111;} " +
-                    "#WikiMonkeyLog pre.timestamp, #WikiMonkeyLog pre.message {overflow:visible; resize:none;} " +
-                    "#WikiMonkeyLog pre.timestamp {float:left; width:5em; margin:0; border:none; padding:0; font-size:0.9em; color:#eee; background-color:transparent;} " +
-                    "#WikiMonkeyLog pre.message {margin:0 0 0.5em 5em; border:none; padding:0; color:#eee; background-color:transparent;} " +
-                    "#WikiMonkeyLog pre.mdebug {color:cyan;} " +
+                    "#WikiMonkeyLog p.timestamp, #WikiMonkeyLog p.message {border:none; padding:0; font-family:monospace; color:#eee;} " +
+                    "#WikiMonkeyLog p.timestamp {float:left; width:5em; margin:0 -5em 0 0; font-size:0.9em;} " +
+                    "#WikiMonkeyLog p.message {margin:0 0 0.5em 5em;} " +
+                    "#WikiMonkeyLog p.mdebug {color:cyan;} " +
                     // The .warning and .error classes are already used by
                     // MediaWiki, without associating them with an id and a tag
-                    "#WikiMonkeyLog pre.mwarning {color:gold;} " +
-                    "#WikiMonkeyLog pre.merror {color:red;}");
-        
+                    "#WikiMonkeyLog p.mwarning {color:gold;} " +
+                    "#WikiMonkeyLog p.merror {color:red;}");
+
         return log;
     };
-    
+
     var appendMessage = function (text, type) {
-        var tstamp = document.createElement('pre');
+        var tstamp = document.createElement('p');
         tstamp.className = 'timestamp';
         var now = new Date();
         tstamp.innerHTML = now.toLocaleTimeString();
-        
-        var msg = document.createElement('pre');
+
+        var msg = document.createElement('p');
         msg.className = 'message' + ((type) ? " " + type : "");
         // Do not allow the empty string, otherwise the resulting html element
         // may not be rendered by the browser
         msg.innerHTML = (text) ? text : " ";
-        
+
         var line = document.createElement('div');
         line.appendChild(tstamp);
         line.appendChild(msg);
-        
+
         var log = document.getElementById('WikiMonkeyLog');
-        
+
         test = log.scrollTop + log.clientHeight == log.scrollHeight;
-        
+
         log.appendChild(line);
-        
+
         if (test) {
             log.scrollTop = log.scrollHeight - log.clientHeight;
         }
     };
-    
+
     this.logDebug = function (text) {
         appendMessage(text, 'mdebug');
     };
-    
+
     this.logInfo = function (text) {
         appendMessage(text);
     };
-    
+
     this.logWarning = function (text) {
         appendMessage(text, 'mwarning');
     };
-    
+
     this.logError = function (text) {
         appendMessage(text, 'merror');
     };
