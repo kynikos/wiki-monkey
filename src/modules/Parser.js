991bc246ee4173f70f367daf7423a9e9aa8068fd
Dario Giovannetti <dev@dariogiovannetti.net>
2013-10-06 15:28:30 +0800

    Parser: improve flexibility and recognize link fragments

--- src/modules/Parser.js
+++ src/modules/Parser.js
@@ -1,19 +1,19 @@
 /*
  *  Wiki Monkey - MediaWiki bot and editor assistant that runs in the browser
  *  Copyright (C) 2011-2013 Dario Giovannetti <dev@dariogiovannetti.net>
- * 
+ *
  *  This file is part of Wiki Monkey.
- * 
+ *
  *  Wiki Monkey is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
- * 
+ *
  *  Wiki Monkey is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
- * 
+ *
  *  You should have received a copy of the GNU General Public License
  *  along with Wiki Monkey.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -22,11 +22,11 @@ WM.Parser = new function () {
     this.convertUnderscoresToSpaces = function (title) {
         return title.replace(/_/g, " ");
     };
-    
+
     this.convertSpacesToUnderscores = function (title) {
         return title.replace(/ /g, "_");
     };
-    
+
     this.neutralizeNowikiTags = function (source) {
         // /<nowiki>[.\s]+?<\/nowiki>/gi doesn't work
         var tags = Alib.RegEx.matchAll(source, /<nowiki>(.?\s?)+?<\/nowiki>/gi);
@@ -36,7 +36,7 @@ WM.Parser = new function () {
         }
         return source;
     };
-    
+
     var prepareTitleCasing = function (pattern) {
         var firstChar = pattern.charAt(0);
         var fcUpper = firstChar.toUpperCase();
@@ -46,7 +46,7 @@ WM.Parser = new function () {
         }
         return pattern;
     };
-    
+
     this.findBehaviorSwitches = function (source, word) {
         source = this.neutralizeNowikiTags(source);
         var regExp;
@@ -60,42 +60,53 @@ WM.Parser = new function () {
         }
         return Alib.RegEx.matchAll(source, regExp);
     };
-    
-    this.findInternalLinks = function (source, namespace) {
+
+    this.findInternalLinks = function (source, namespace, title) {
         source = this.neutralizeNowikiTags(source);
         var regExp;
+
         if (namespace) {
-            // Namespaces aren't case-sensitive
-            regExp = new RegExp("\\[\\[:?[ _]*:?[ _]*((" + Alib.RegEx.escapePattern(namespace) + ")[ _]*:[ _]*(.+?)(?:[ _]*\\|\\s*(.+?))?)\\s*\\]\\]", "gi");
+            if (title) {
+                // Namespaces wouldn't be case-sensitive, but titles are, so be safe and use only the g flag
+                regExp = new RegExp("\\[\\[:?[ _]*:?[ _]*((" + Alib.RegEx.escapePattern(namespace) + ")[ _]*:[ _]*((" + Alib.RegEx.escapePattern(title) + ")(?:[ _]*#(.+?))?)(?:[ _]*\\|\\s*(.+?))?)\\s*\\]\\]", "g");
+            }
+            else {
+                // Namespaces aren't case-sensitive
+                regExp = new RegExp("\\[\\[:?[ _]*:?[ _]*((" + Alib.RegEx.escapePattern(namespace) + ")[ _]*:[ _]*((.+?)(?:[ _]*#(.+?))?)(?:[ _]*\\|\\s*(.+?))?)\\s*\\]\\]", "gi");
+            }
+        }
+        else if (title) {
+            // Titles are case-sensitive
+            // Note the () that represents the missing namespace in order to keep the match indices consistent with the other regular expressions
+            regExp = new RegExp("\\[\\[:?[ _]*:?[ _]*(()((" + Alib.RegEx.escapePattern(title) + ")(?:[ _]*#(.+?))?)(?:[ _]*\\|\\s*(.+?))?)\\s*\\]\\]", "g");
         }
         else {
-            // Namespaces aren't case-sensitive
-            regExp = /\[\[:?[ _]*:?[ _]*((?:(.+?)[ _]*:[ _]*)?(.+?)(?:[ _]*\|\s*(.+?))?)\s*\]\]/gi;
+            regExp = /\[\[:?[ _]*:?[ _]*((?:(.+?)[ _]*:[ _]*)?((.+?)(?:[ _]*#(.+?))?)(?:[ _]*\|\s*(.+?))?)\s*\]\]/g;
         }
         return Alib.RegEx.matchAll(source, regExp);
     };
-    
+
     this.findInterwikiLinks = function (source, wiki) {
         return this.findInternalLinks(source, wiki);
     };
-    
+
     this.findSpecialLinks = function (source, pattern) {
         // See also WM.ArchWiki.findAllInterlanguageLinks!!!
         source = this.neutralizeNowikiTags(source);
         // Categories and language tags aren't case-sensitive
-        var regExp = new RegExp("\\[\\[(?:[ _]+:)?[ _]*((?:(" + pattern + ")[ _]*:[ _]*)(.+?)(?:[ _]*\\|\\s*(.+?))?)\\s*\\]\\]", "gi");
+        var regExp = new RegExp("\\[\\[(?:[ _]+:)?[ _]*((?:(" + pattern + ")[ _]*:[ _]*)((.+?)(?:[ _]*#(.+?))?)(?:[ _]*\\|\\s*(.+?))?)\\s*\\]\\]", "gi");
         return Alib.RegEx.matchAll(source, regExp);
     };
-    
+
     this.findCategories = function (source) {
         return this.findSpecialLinks(source, "Category");
     };
-    
+
     this.findInterlanguageLinks = function (source, language) {
         // See also WM.ArchWiki.findAllInterlanguageLinks!!!
         return this.findSpecialLinks(source, Alib.RegEx.escapePattern(language));
     };
-    
+
     this.findVariables = function (source, variable) {
         source = this.neutralizeNowikiTags(source);
         // Variables are case-sensitive
@@ -104,14 +115,14 @@ WM.Parser = new function () {
         var regExp = new RegExp("\\{\\{\\s*((" + Alib.RegEx.escapePattern(variable) + ")(?:\\:[_\\s]*((?:.(?!\\{\\{)[_\\s]*?)+?))?)[_\\s]*\\}\\}", "g");
         return Alib.RegEx.matchAll(source, regExp);
     };
-    
+
     var findTransclusionsEngine = function (source, regExp) {
         var nSource = WM.Parser.neutralizeNowikiTags(source);
         var transclusions = [];
-        
+
         do {
             var res = Alib.RegEx.matchAll(nSource, regExp);
-            
+
             for (var t in res) {
                 var match = res[t].match;
                 var index = res[t].index;
@@ -121,7 +132,7 @@ WM.Parser = new function () {
                     var args = match[3].split("|");
                     // 1 is the length of |
                     var argId = index + match[1].length + 1;
-                    
+
                     for (var a in args) {
                         var argL = args[a].length;
                         var eqId = args[a].indexOf("=");
@@ -132,7 +143,7 @@ WM.Parser = new function () {
                             var keyMatches = reKey.exec(rawKey);
                             var key = keyMatches[2];
                             var keyId = argId + ((keyMatches[1]) ? keyMatches[1].length : 0);
-                            
+
                             // 1 is the length of =
                             var nValue = args[a].substr(eqId + 1);
                             var valueId = argId + keyMatches[0].length + 1;
@@ -145,19 +156,19 @@ WM.Parser = new function () {
                             var valueId = argId;
                             var valueL = argL;
                         }
-                        
+
                         var value = source.substr(valueId, valueL);
-                        
+
                         arguments.push({key: key,
                                         key_index: keyId,
                                         value: value,
                                         value_index: valueId});
-                        
+
                         // 1 is the length of |
                         argId = argId + argL + 1;
                     }
                 }
-                
+
                 transclusions.push({
                     title: match[2],
                     match: match,
@@ -165,16 +176,16 @@ WM.Parser = new function () {
                     length: L,
                     arguments: arguments,
                 });
-                
+
                 var filler = Alib.Str.padRight("", "x", L);
                 nSource = Alib.Str.overwriteAt(nSource, filler, res[t].index);
             }
         // Find also nested transclusions
         } while (res.length);
-        
+
         return transclusions;
     };
-    
+
     this.findTemplates = function (source, template) {
         // Templates can't be transcluded with a colon before the title
         // The title must not be broken by new line characters
@@ -188,7 +199,7 @@ WM.Parser = new function () {
         var regExp = new RegExp("(\\{\\{\\s*[_ ]*(" + pattern + ")[_\\s]*)(?:\\|((?:\\s*.(?!\\{\\{)\\s*)*?))?\\}\\}", "g");
         return findTransclusionsEngine(source, regExp);
     };
-    
+
     this.findTransclusions = function (source, namespace, title) {
         // The difference from templates is the possibility of a colon before
         // the title which forces the transclusion of a page instead of a
@@ -202,7 +213,7 @@ WM.Parser = new function () {
             var titlePattern = Alib.RegEx.escapePattern(title);
             titlePattern = prepareTitleCasing(titlePattern);
         }
-        
+
         if (namespacePattern && titlePattern) {
             var pattern = namespacePattern + "[ _]*:[ _]*" + titlePattern;
         }
@@ -215,26 +226,26 @@ WM.Parser = new function () {
         else {
             var pattern = ".+?";
         }
-        
+
         // There can't be an underscore before the colon
         var regExp = new RegExp("(\\{\\{\\s*:?[ _]*(" + pattern + ")[_\\s]*)(?:\\|((?:\\s*.(?!\\{\\{)\\s*)*?))?\\}\\}", "g");
         return findTransclusionsEngine(source, regExp);
     };
-        
+
     this.findSectionHeadings = function (source) {
         // ======Title====== is the deepest level supported
         var MAXLEVEL = 6;
-        
+
         var sections = [];
         var minLevel = MAXLEVEL;
         var maxTocLevel = 0;
         var tocLevel = 1;
         var regExp = /^(\=+ *.+? *\=+)[ \t]*$/gm;
         var match, line, L0, L1, level, prevLevels, start, end, tocPeer;
-        
+
         while (true) {
             match = regExp.exec(source);
-            
+
             if (match) {
                 L0 = match[0].length;
                 line = match[1];
@@ -242,20 +253,20 @@ WM.Parser = new function () {
                 level = 1;
                 start = "=";
                 end = "=";
-                
+
                 // ==Title=== and ===Title== are both 2nd levels and so on
                 // (the shortest sequence of = between the two sides is
                 //  considered)
-                
+
                 // = and == are not titles
                 // === is read as =(=)=, ==== is read as =(==)= (both 1st
                 //                                               levels)
                 // ===== is read as ==(=)== (2nd level) and so on
-                
+
                 while (true) {
                     start = line.substr(level, 1);
                     end = line.substr(L1 - level - 1, 1);
-                    
+
                     if (L1 - level * 2 > 2 && start == "=" && end == "=") {
                         level++;
                     }
@@ -269,7 +280,7 @@ WM.Parser = new function () {
                         break;
                     }
                 }
-                
+
                 if (level == minLevel) {
                     tocLevel = 1;
                     prevLevels = {};
@@ -303,7 +314,7 @@ WM.Parser = new function () {
                     }
                     prevLevels.relMax = level;
                 }
-                
+
                 sections.push({line: line,
                                level: level,
                                tocLevel: tocLevel,
@@ -315,12 +326,12 @@ WM.Parser = new function () {
                 break;
             }
         }
-        
+
         // Articles without sections
         if (maxTocLevel == 0) {
             minLevel = 0;
         }
-        
+
         return {sections: sections,
                 minLevel: minLevel,
                 maxTocLevel: maxTocLevel};
