e602b47584d820cac510ab134f045db766cbfeec
Dario Giovannetti <dev@dariogiovannetti.net>
2013-12-07 19:44:37 +0800

    FixBacklinkFragments: fix related articles + bugfixes
    Fixes #117

--- src/plugins/FixBacklinkFragments.js
+++ src/plugins/FixBacklinkFragments.js
@@ -20,43 +20,139 @@
 
 WM.Plugins.FixBacklinkFragments = new function () {
     var fixLinks = function (source, target, sections) {
-        var newString = source;
-
         // Note that it's impossible to recognize any namespaces in the title without querying the server
         // Alternatively, a list of the known namespaces could be maintained for each wiki
         // Recognizing namespaces would let recognize more liberal link syntaxes (e.g. spaces around the colon)
         var links = WM.Parser.findInternalLinks(source, null, target);
 
+        var newText = "";
+        var prevId = 0;
+
         for (var l = 0; l < links.length; l++) {
             var link = links[l];
+
+            newText += source.substring(prevId, link.index);
+            var newlink = link.match[0];
+
             var rawfragment = link.match[5];
 
             if (rawfragment) {
-                var fragment = WM.Parser.squashContiguousWhitespace(rawfragment).trim();
+                var fixedFragment = fixFragment(rawfragment, sections);
 
-                if (sections.indexOf(fragment) < 0) {
-                    var fixed = false;
+                if (fixedFragment === true) {}
+                else if (fixedFragment) {
+                    var oldlink = newlink;
+                    newlink = "[[" + target + "#" + fixedFragment + ((link.match[6]) ? "|" + link.match[6] : "") + "]]";
+                    WM.Log.logInfo("Fixed broken link fragment: " + oldlink + " -> " + newlink);
+                }
+                else {
+                    WM.Log.logWarning("Cannot fix broken link fragment: " + newlink);
+                }
+            }
 
-                    for (var s = 0; s < sections.length; s++) {
-                        var section = sections[s];
+            newText += newlink;
+            prevId = link.index + link.length;
+        }
+        newText += source.substr(prevId);
 
-                        if (section.toLowerCase() == fragment.toLowerCase()) {
-                            var newlink = "[[" + target + "#" + section + ((link.match[6]) ? "|" + link.match[6] : "") + "]]";
-                            newString = Alib.Str.overwriteFor(newString, newlink, link.index, link.length);
-                            WM.Log.logInfo("Fixed broken link fragment: " + link.match[0] + " -> " + newlink);
-                            fixed = true;
-                            break;
-                        }
+        // Without this check this plugin would be specific to ArchWiki
+        if (location.hostname == 'wiki.archlinux.org') {
+            newText = fixArchWikiLinks(newText, target, sections);
+        }
+
+        return newText;
+    };
+
+    var fixArchWikiLinks = function (source, target, sections) {
+        var links = WM.Parser.findTemplates(source, 'Related');
+
+        var newText1 = "";
+        var prevId = 0;
+
+        for (var l = 0; l < links.length; l++) {
+            newText1 += source.substring(prevId, links[l].index);
+            newText1 += fixArchWikiLink(target, sections, links[l], 1);
+            prevId = links[l].index + links[l].length;
+        }
+        newText1 += source.substr(prevId);
+
+        var links2 = WM.Parser.findTemplates(newText1, 'Related2');
+
+        var newText2 = "";
+        var prevId = 0;
+
+        for (var l = 0; l < links2.length; l++) {
+            newText2 += newText1.substring(prevId, links2[l].index);
+            newText2 += fixArchWikiLink(target, sections, links2[l], 2);
+            prevId = links2[l].index + links2[l].length;
+        }
+        newText2 += newText1.substr(prevId);
+
+        return newText2;
+    };
+
+    var fixArchWikiLink = function (target, sections, template, expectedArgs) {
+        var args = template.arguments;
+
+        // Don't crash in case of malformed templates
+        if (args.length == expectedArgs) {
+            var link = args[0].value;
+            var fragId = link.indexOf('#');
+
+            if (fragId > -1) {
+                var ltitle = link.substring(0, fragId);
+
+                // Note that it's impossible to recognize any namespaces in the title without querying the server
+                // Alternatively, a list of the known namespaces could be maintained for each wiki
+                // Recognizing namespaces would let recognize more liberal link syntaxes (e.g. spaces around the colon)
+                if (WM.Parser.compareArticleTitles(ltitle, target)) {
+                    var rawfragment = link.substr(fragId + 1);
+                    var fixedFragment = fixFragment(rawfragment, sections);
+
+                    if (fixedFragment === true) {
+                        // Don't do anything in this case
                     }
-
-                    if (!fixed) {
-                        WM.Log.logWarning("Cannot fix broken link fragment: " + link.match[0]);
+                    else if (fixedFragment) {
+                        var anchor = (args[1]) ? ("|" + args[1].value) : "";
+                        var newlink = "{{" + template.title + "|" + target + "#" + fixedFragment  + anchor + "}}";
+                        WM.Log.logInfo("Fixed broken link fragment: " + template.match[0] + " -> " + newlink);
+                        return newlink;
+                    }
+                    else {
+                        WM.Log.logWarning("Cannot fix broken link fragment: " + template.match[0]);
                     }
                 }
             }
         }
+        else {
+            WM.Log.logWarning("Template:" + template.title + " must have " + expectedArgs + " and only " +
+            expectedArgs + ((expectedArgs > 1) ? " arguments: " : " argument: ") + template.match[0]);
+        }
 
-        return newString;
+        return template.match[0];
+    };
+
+    var fixFragment = function (rawfragment, sections) {
+        if (rawfragment) {
+            var fragment = WM.Parser.squashContiguousWhitespace(rawfragment).trim();
+
+            if (sections.indexOf(fragment) < 0) {
+                for (var s = 0; s < sections.length; s++) {
+                    var section = sections[s];
+
+                    if (section.toLowerCase() == fragment.toLowerCase()) {
+                        return section;
+                    }
+                }
+                return false;
+            }
+            else {
+                return true;
+            }
+        }
+        else {
+            return true;
+        }
     };
 
     this.mainAuto = function (args, title, callBot, chainArgs) {
@@ -66,8 +162,15 @@ WM.Plugins.FixBacklinkFragments = new function () {
             var params = {
                 'action': 'parse',
                 'prop': 'sections',
-                'page':target
+                'page':target,
+                'redirects': 1,
             };
+            WM.Log.logWarning("If some articles in the list are linking to this article " +
+                              "through a redirect, you should process the backlinks of that " +
+                              "redirect page separately through its Special:WhatLinksHere " +
+                              "page, as this plugin can only fix links that exactly match " +
+                              "the title of this article.\nIn order to save time you are " +
+                              "advised to hide the redirects in this list.");
 
             WM.MW.callAPIGet(params,
                              null,
