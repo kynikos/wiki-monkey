722efb6f5e120934fcd57b050b34a9ac9e14b82c
Dario Giovannetti <dev@dariogiovannetti.net>
2014-10-02 12:46:29 +0800

    Create compatibility configurations

---
+++ src/configurations/chromium/WikiMonkey-archwikibot-chromium.meta.js
@@ -0,0 +1,3 @@
+// ==UserScript==
+// @version 1.16.0-dev-archwikibot-chromium
+// ==/UserScript==
---
+++ src/configurations/opera/WikiMonkey-archwikieditor-opera.meta.js
@@ -0,0 +1,3 @@
+// ==UserScript==
+// @version 1.16.0-dev-archwikieditor-opera
+// ==/UserScript==
