dd576ed8830cb7ace15a808edd2c999846f077ad
Dario Giovannetti <dev@dariogiovannetti.net>
2016-03-06 14:35:13 +0800

    Update local user.js

--- auxiliary/WikiMonkey-local.user.js
+++ auxiliary/WikiMonkey-local.user.js
@@ -3,7 +3,7 @@
 // @name Wiki Monkey (local)
 // @namespace https://github.com/kynikos/wiki-monkey
 // @author Dario Giovannetti <dev@dariogiovannetti.net>
-// @version 2.0.6-dev-local
+// @version 2.0.7-dev-local
 // @description MediaWiki-compatible bot and editor assistant that runs in the browser (local version)
 // @website https://github.com/kynikos/wiki-monkey
 // @supportURL https://github.com/kynikos/wiki-monkey/issues
