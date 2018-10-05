c77f55b78699d40d2679cd0bdf91a13f38dcb6b0
Dario Giovannetti <dev@dariogiovannetti.net>
2018-10-06 00:57:54 +0800

    Add asciiSpinner component

---
+++ src/app/_components/asciiSpinner/index.js
@@ -0,0 +1,45 @@
+// Wiki Monkey - MediaWiki bot and editor-assistant user script
+// Copyright (C) 2011 Dario Giovannetti <dev@dariogiovannetti.net>
+//
+// This file is part of Wiki Monkey.
+//
+// Wiki Monkey is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Wiki Monkey is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Wiki Monkey.  If not, see <http://www.gnu.org/licenses/>.
+
+
+module.exports.asciiSpinner = {
+  name: 'asciiSpinner',
+
+  data() {
+    return {
+      char: '+',
+    }
+  },
+
+  methods: {
+    changeCharacter() {
+      this.char = {
+        '+': '×',
+        '×': '+',
+      }[this.char]
+    },
+  },
+
+  mounted() {
+    setInterval(this.changeCharacter, 250)
+  },
+
+  render(h) {
+    return h('span', this.char)
+  },
+}
