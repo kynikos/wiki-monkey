402eab26e4afbc8eb51ec6e1bfdfb5c70e575038
Dario Giovannetti <dev@dariogiovannetti.net>
2018-10-30 23:58:59 +0800

    TODO

--- src/lib/DB.js
+++ src/lib/DB.js
@@ -32,6 +32,11 @@ error data in the browser's console.",
 }
 
 // TODO: Always send the client version to the server to check for compatibility?
+//       ...or serve the user script directly from wiki-snake, and only allow
+//       that version to connect to the server; also adapt the auto-upgrade
+//       system to remind updating the server (probably through the AUR package)
+//       At that point it would also make sense to allow loading configuration
+//       also from a dotfile
 
 
 module.exports = class exports {
