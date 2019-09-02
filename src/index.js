22ad92d96505d71cb1624c4f09122807e0899cc9
Dario Giovannetti <dev@dariogiovannetti.net>
2019-09-02 17:34:01 +0800

    Mediawiki 1.33 merged mediawiki.api.* modules into one
    https://gerrit.wikimedia.org/r/434179

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@
 // along with Wiki Monkey.  If not, see <http://www.gnu.org/licenses/>.
 
 const mwmodpromise = mw.loader.using([
-  'mediawiki.api.edit',
+  'mediawiki.api',
   'mediawiki.notification',
   'mediawiki.Title',
   'mediawiki.Uri',
